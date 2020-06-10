375b99ba612c57fa59c004f2314c55d6be8c450e
Abhi02-pro <61277379+Abhi02-pro@users.noreply.github.com>
2020-06-10 13:09:54 +0530

    python basics

---
+++ 1. Basics/dataTypes.py
@@ -0,0 +1,18 @@
+
+# python has 5 data types
+
+# 1. Number        eg: var1 = 5232
+# 2. String        eg: name = "Alex"
+# 3. List          eg: data = [1,2,3,4,5]
+# 4. Tuple         eg: fruit = ("Apple", "Mango", "Banana")
+# 5. Dictionary    eg: roll = {"Ram":1, "Sam":2, "Hero":3}
+
+
+# Number 
+#  - int     store integer values
+#  - float   store decimal or floating point values
+
+# Another type is
+# Boolean
+# - true
+# - false
